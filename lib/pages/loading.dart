0e1c4573fd8f22b718037705b789524cb9da3d61
janvier <iyakaremyejanvier1@gnmail.com>
2023-11-13 15:14:55 +0200

    first commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:world_time/services/world_time.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setWorldTime() async {
+    WorldTime instance = WorldTime(
+      location: 'harare',
+      flag: 'harare.png',
+      url: 'Africa/Harare',
+    );
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    setWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      body: const Center(
+        child: SpinKitSpinningLines(
+          color: Colors.white,
+          size: 100,
+        ),
+      ),
+    );
+  }
+}
